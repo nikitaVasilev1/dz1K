c87e9f14f61a859d43eeb669930af902bf309bac
Nikita Vasilev <nikita.vas@yandex.ru>
2022-03-29 11:26:21 +0300

    mod Second

--- SecondProgram/src/main/kotlin/Main.kt
+++ SecondProgram/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     print("Введите количество лайков: ")
     val likes: Int = readLine()!!.toInt()
-    if (likes % 10 > 1)
+    if (likes % 10 > 1 || (likes > 100 && likes % 10 == 1) || likes < 20 )
         println("Понравилось $likes людям") else println("Понравилось $likes человеку")
 }
